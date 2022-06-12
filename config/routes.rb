af804c0063fef0000f829c4784caf49ea428ccc6
ruirui0810 <jobonly2525@gmail.com>
2022-06-12 19:08:33 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update]
+  resources :users, only: [:edit, :update]
+
 end
